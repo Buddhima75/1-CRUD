9ad0c44f4668068a8b0d82010c183367f21e9888
Buddhima75 <buddima60@gmail.com>
2025-05-18 03:16:32 +0530

    MERN Project

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import dotenv from "dotenv";
+import { connectDB } from './config/db.js';
+
+import productRoutes from "./routes/product.route.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // allow to accept json data to body
+
+
+app.use("/api/products", productRoutes);
+
+app.listen(PORT, () =>  {
+    connectDB();
+    console.log("server started at http://localhost:" + PORT);
+});
+// vqai5fSRJ0ZN5yc8
